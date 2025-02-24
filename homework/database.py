41f31219ae3181483a5e2b6e65ed58d447aa0d67
Denis <denis181079@xmail.ru>
2025-02-24 11:45:15 +0700

    Create database.py

---
+++ homework/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+# expire_on_commit=False will prevent attributes from being expired
+# after commit.
+async_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+session = async_session()
+class Base(DeclarativeBase):
+    pass
