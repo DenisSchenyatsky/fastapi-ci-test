07eebd533af9bce881aee2a2c9ac3d571b258a19
Denis <denis181079@xmail.ru>
2025-02-24 11:46:32 +0700

    Create db_create.py

---
+++ homework/db_create.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+txt ="""-- Рецепты
+CREATE TABLE IF NOT EXISTS recipes (
+id integer PRIMARY KEY,
+name text NOT NULL,
+time_spent text,
+description text,
+watched_count integer
+);
+
+-- Продукты
+CREATE TABLE IF NOT EXISTS products (
+id integer PRIMARY KEY,
+name text UNIQUE NOT NULL
+);
+
+-- Парасочетания предыдущих двух
+CREATE TABLE IF NOT EXISTS products_recipes (
+recipe_id integer NOT NULL,
+product_id integer NOT NULL,
+FOREIGN KEY (recipe_id) REFERENCES recipes(id),
+FOREIGN KEY (product_id) REFERENCES products(id),
+PRIMARY KEY(recipe_id, product_id)
+);"""
+
+with sqlite3.connect("recipes.db") as conn:
+    cursor = conn.cursor()
+    cursor.executescript(txt)
+    conn.commit()
