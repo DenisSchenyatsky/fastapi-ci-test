ed3fc9cee3f65aceee4a237a4138d1a1e8ecf1a1
Denis <denis181079@xmail.ru>
2025-02-24 13:48:05 +0700

    Update main.py

--- homework/main.py
+++ homework/main.py
@@ -13,6 +13,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logging.info("---===< DB and its ORM try to init...>===---")
@@ -23,42 +24,50 @@ async def lifespan(app: FastAPI):
     await session.close()
     await engine.dispose()
 
+
 app = FastAPI(lifespan=lifespan)
 
 
-
-@app.post('/recipes/', response_model=schemas.RecipeOut)
+@app.post("/recipes/", response_model=schemas.RecipeOut)
 async def recipes(recipe_schema: schemas.RecipeIn) -> models.Recipe:
     """
     <h1>Внесение нового рецепта в БД.</h1>
     """
     r_d = recipe_schema.model_dump(exclude_unset=True)
     new_recipe = models.Recipe(**r_d)
-    
+
     async with session.begin():
         await new_recipe.re_create()
         session.add(new_recipe)
-        
+
     return new_recipe
 
 
-@app.get('/recipes/', response_model=List[schemas.RecipeOutSimple])
+@app.get("/recipes/", response_model=List[schemas.RecipeOutSimple])
 async def recipes() -> List[models.Recipe]:
     """
     <h1>Получение списка всех рецептов.</h1>
     """
-    res = await session.execute(select(models.Recipe).order_by(desc(models.Recipe.watched_count), models.Recipe.name))
+    res = await session.execute(
+        select(models.Recipe).order_by(
+            desc(models.Recipe.watched_count), models.Recipe.name
+        )
+    )
     objs = res.scalars().all()
     return objs
 
 
-@app.get('/recipes{recipe_id}/', response_model=schemas.RecipeOut)
+@app.get("/recipes{recipe_id}/", response_model=schemas.RecipeOut)
 async def recipes(recipe_id: int = 1) -> models.Recipe:
     """
     <h1>Получение детальной информации приготовления по id.</h1>
     """
-    res = await session.execute(select(models.Recipe).join(models.ProductRecipe).join(models.Product)
-                                .where(models.Recipe.id==recipe_id))
+    res = await session.execute(
+        select(models.Recipe)
+        .join(models.ProductRecipe)
+        .join(models.Product)
+        .where(models.Recipe.id == recipe_id)
+    )
     recipe = res.scalar()
     recipe.watched_count += 1
     await session.commit()
